c86ea7b0d6896ae5bd94c70001d183b74da75b2a
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-17 19:16:02 -0300

    images: configure remote image loading from imagekit.io

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
