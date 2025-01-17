55e6df7b0e0dd1a066f2e252a2333c8d7448cb1e
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-17 20:05:43 -0300

    style: update category items layout and typography for improved readability

--- src/components/Aside/Aside.styled.ts
+++ src/components/Aside/Aside.styled.ts
@@ -3,8 +3,52 @@ import styled from 'styled-components';
 export const StyledAside = styled.aside`
   background: #0f0f11;
   min-height: 100%;
-  width: 400px;
+  width: 300px;
   border-right: 1px solid #3d3d3d;
   color: #fff;
-  padding: 20px;
+
+  .logo-wrapper {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding: 20px 30px;
+  }
+
+  .catetory-text {
+    padding: 0 47px;
+    font-size: 14px;
+    margin: 30px 0 20px 0;
+  }
+`;
+
+export const StyledList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  font-size: 18px;
+  color: #333;
+`;
+
+export const StyledListItem = styled.li`
+  margin: 0 auto;
+  background-color: #14b04f;
+  margin: 8px auto;
+  padding: 15px 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
+  color: #fff;
+  border: 1px solid #3d3d3d;
+  border-radius: 40px;
+  width: 250px;
+  transition: 0.2s;
+  color: #fff;
+  font-weight: bolder;
+
+  &:hover {
+    border: 1px solid #0aa043;
+    transform: scale(1.03);
+    background: #0aa043;
+  }
 `;
--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,13 +1,19 @@
 import { LOGO } from '@/constants/images';
-import { StyledAside } from './Aside.styled';
+import { StyledAside, StyledList, StyledListItem } from './Aside.styled';
 import Image from 'next/image';
 
 function Aside() {
   return (
     <StyledAside>
-      <div>
+      <div className="logo-wrapper">
         <Image src={LOGO} alt="HunterStack Logo" width={50} height={44} priority />
       </div>
+      <p className="catetory-text">Categorias</p>
+      <StyledList>
+        <StyledListItem>Pessoas</StyledListItem>
+        <StyledListItem>Filmes</StyledListItem>
+        <StyledListItem>Planetas</StyledListItem>
+      </StyledList>
     </StyledAside>
   );
 }
