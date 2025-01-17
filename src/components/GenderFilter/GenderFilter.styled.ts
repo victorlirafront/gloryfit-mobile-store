1fc136e8fb1730d18d27d10845f13562b3853f8f
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-17 19:12:21 -0300

    feat: add Genderfilter component

---
+++ src/components/GenderFilter/GenderFilter.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledGenderFilter = styled.div`
+  background: #0f0f11;
+  height: 100px;
+  width: 100%;
+  color: #fff;
+  border-top: 1px solid #3d3d3d;
+  padding: 20px;
+`;
---
+++ src/components/GenderFilter/GenderFilter.tsx
@@ -0,0 +1,12 @@
+import SelectCategory from '../SelectCategory/SelectCategory';
+import { StyledGenderFilter } from './GenderFilter.styled';
+
+function GenderFilter() {
+  return (
+    <StyledGenderFilter>
+      <SelectCategory />
+    </StyledGenderFilter>
+  );
+}
+
+export default GenderFilter;
