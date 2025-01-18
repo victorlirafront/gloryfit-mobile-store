8c995c9025f3188ab33e608c6a01e07b719ae78d
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-18 16:29:29 -0300

    refactor: remove promise constructor from helper function

--- src/helper/scrollToTop/scrollToTop.ts
+++ src/helper/scrollToTop/scrollToTop.ts
@@ -1,10 +1,6 @@
 export const scrollToTop = () => {
-  return new Promise((resolve) => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-
-    setTimeout(resolve, 500);
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
   });
 };
