b2682cdd67234ba02dea8c1ee112b85c6f95343c
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-17 23:54:47 -0300

    feat: add Footer component

---
+++ src/components/Footer/Footer.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  height: 100px;
+  color: #fff;
+  background: #0f0f11;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { StyledFooter } from './Footer.styled';
+
+function Footer() {
+  return (
+    <StyledFooter>
+      <p>Desenvolvido por: Victor Lira</p>
+    </StyledFooter>
+  );
+}
+
+export default Footer;
