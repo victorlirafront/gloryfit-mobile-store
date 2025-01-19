bfd2c66b232777e0cc1ad7af5228048340cf7b81
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-19 19:45:03 -0300

    refactor: remove footer aos animation and calc card min height

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { StyledFooter } from './Footer.styled';
 
 function Footer() {
   return (
-    <StyledFooter data-aos="fade-down" data-aos-duration="600" data-aos-offset="0">
+    <StyledFooter>
       <p>Desenvolvido por: Victor Lira</p>
     </StyledFooter>
   );
