1a20aa8a21be495cf1081749022184c630367f63
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-18 23:35:51 -0300

    style: add footer aos offset

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { StyledFooter } from './Footer.styled';
 
 function Footer() {
   return (
-    <StyledFooter data-aos="fade-down" data-aos-duration="600">
+    <StyledFooter data-aos="fade-down" data-aos-duration="600" data-aos-offset="0">
       <p>Desenvolvido por: Victor Lira</p>
     </StyledFooter>
   );
