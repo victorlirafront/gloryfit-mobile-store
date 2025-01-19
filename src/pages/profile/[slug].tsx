0d142a73c7b98b3d7f1b807a9156764f32848672
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-01-19 15:48:41 -0300

    remove display component from dynamic page

--- src/pages/profile/[slug].tsx
+++ src/pages/profile/[slug].tsx
@@ -2,62 +2,11 @@ import { SWAPI } from '@/constants/endpoints';
 import { wrapper } from '@/redux/store';
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
-import { StyledProfile, StyledProfileWrapper } from './profile.styled';
-import Image from 'next/image';
-import { BACK_ARROW } from '@/constants/images';
-import Link from 'next/link';
-import { Film, Person, Planet } from '@/types/swapi';
-import { ProfileProps } from './profile.types';
-
-const renderProfileData = (data: Person | Planet | Film | null) => {
-  if (!data) return <p>No data available</p>;
-
-  return (
-    <div>
-      {Object.entries(data).map(([key, value]) => (
-        <p key={key} style={{ marginTop: 10 }}>
-          <strong style={{ color: '#d2dd52' }}>{key}:</strong>{' '}
-          {Array.isArray(value) ? (
-            value.join(', ')
-          ) : typeof value === 'string' && value.startsWith('http') ? (
-            <a href={value} target="_blank" rel="noopener noreferrer">
-              {value}
-            </a>
-          ) : (
-            value
-          )}
-        </p>
-      ))}
-    </div>
-  );
-};
+import DynamicInformations from '@/components/DynamicInformations/DynamicInformations';
+import { ProfileProps } from '@/components/DynamicInformations/DynamicInformations.types';
 
 const Profile = ({ data }: ProfileProps) => {
-  return (
-    <StyledProfileWrapper>
-      <StyledProfile>
-        <header className="profile-header">
-          <Link href="/">
-            <div className="back-arrow-wrapper">
-              <Image
-                className="profile-back-arrow"
-                alt="arrow back"
-                width={30}
-                height={30}
-                src={BACK_ARROW}
-              />
-              <p>Go back</p>
-            </div>
-          </Link>
-        </header>
-        <main className="profile-main">
-          <h1>Details</h1>
-          <div className="background-image"></div>
-          {renderProfileData(data)}
-        </main>
-      </StyledProfile>
-    </StyledProfileWrapper>
-  );
+  return <DynamicInformations data={data} />;
 };
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(
