b65a0814ed0ba8aefc281d686bf63d1e0d913cd1
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-02-22 14:42:45 -0300

    feat: add Header and Footer placeholder

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+function Footer(){
+  return <View>Footer</View>
+}
+
+export default Footer
---
+++ src/Header/Header.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+function Header(){
+  return <View>Header</View>
+}
+
+export default Header
